336dc5e8882d99e78ef87ffcf1a93c249fde993f
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-07-26 15:01:14 +0200

    feat: add tsvector columns for full-text search

---
+++ migrations/0.1.1_add_search_vectors.sql
@@ -0,0 +1,31 @@
+ALTER TABLE paragraphs
+ADD COLUMN search tsvector,
+ADD COLUMN reindex boolean DEFAULT false;
+
+CREATE OR REPLACE FUNCTION paragraphs_search_trigger() RETURNS trigger AS $$
+begin
+    IF new.reindex THEN
+        new.search := to_tsvector('german', new.body);
+        new.reindex := false;
+    END IF;
+    return new;
+end
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER paragraphs_search_trigger BEFORE INSERT OR UPDATE
+ON paragraphs FOR EACH ROW EXECUTE PROCEDURE paragraphs_search_trigger();
+
+ALTER TABLE sentences
+ADD COLUMN search tsvector,
+ADD COLUMN reindex boolean DEFAULT false;
+
+CREATE OR REPLACE FUNCTION sentences_search_trigger() RETURNS trigger AS $$
+begin
+    IF new.reindex THEN
+        new.search := to_tsvector('german', new.body);
+        new.reindex := false;
+    END IF;
+    return new;
+end
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER sentences_search_trigger BEFORE INSERT OR UPDATE
+ON paragraphs FOR EACH ROW EXECUTE PROCEDURE sentences_search_trigger();
