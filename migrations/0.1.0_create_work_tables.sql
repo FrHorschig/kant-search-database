df06ae1f528b1c6d6acde00fbbc6a4377fa5f0ba
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-07-28 18:06:07 +0200

    feat: remove unique constraint from abbreviation

--- migrations/0.1.0_create_work_tables.sql
+++ migrations/0.1.0_create_work_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS works (
     id SERIAL PRIMARY KEY,
     title VARCHAR NOT NULL UNIQUE,
-    abbreviation VARCHAR UNIQUE,
+    abbreviation VARCHAR,
     volume INTEGER NOT NULL,
     ordinal INTEGER,
     year INTEGER
