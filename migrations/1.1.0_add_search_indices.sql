feb81c05d7edd347b562277d23d77be104d07ac9
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-09-15 10:52:51 +0200

    fix: fix syntax errors and duplicated titles

--- migrations/1.1.0_add_search_indices.sql
+++ migrations/1.1.0_add_search_indices.sql
@@ -14,18 +14,18 @@ CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
 ON paragraphs FOR EACH ROW EXECUTE PROCEDURE
 content_trigger();
 
-ALTER TABLE sentences
-ADD COLUMN search tsvector DEFAULT to_tsvector('german', '');
-CREATE INDEX search_idx ON sentences USING GIN (search);
-CREATE OR REPLACE FUNCTION content_trigger() RETURNS trigger AS $$
-BEGIN
-  NEW.search := to_tsvector('german', 
-                regexp_replace(
-                  regexp_replace(NEW.content, '<\/?[^>]*>', '', 'g'), 
-                '{[^}]*}', '', 'g'));
-  return NEW;
-END
-$$ LANGUAGE plpgsql;
-CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
-ON sentences FOR EACH ROW EXECUTE PROCEDURE
-content_trigger();
+-- ALTER TABLE sentences
+-- ADD COLUMN search tsvector DEFAULT to_tsvector('german', '');
+-- CREATE INDEX search_idx ON sentences USING GIN (search);
+-- CREATE OR REPLACE FUNCTION content_trigger() RETURNS trigger AS $$
+-- BEGIN
+--   NEW.search := to_tsvector('german', 
+--                 regexp_replace(
+--                   regexp_replace(NEW.content, '<\/?[^>]*>', '', 'g'), 
+--                 '{[^}]*}', '', 'g'));
+--   return NEW;
+-- END
+-- $$ LANGUAGE plpgsql;
+-- CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
+-- ON sentences FOR EACH ROW EXECUTE PROCEDURE
+-- content_trigger();
--- migrations/1.2.0_insert_volumes_and_works.sql
+++ migrations/1.2.0_insert_volumes_and_works.sql
@@ -1,4 +1,4 @@
-INSERT INTO TABLE volumes(id, title, section) VALUES 
+INSERT INTO volumes(id, title, section) VALUES 
 (1, 'Vorkritische Schriften I: 1747–1756', 1),
 (2, 'Vorkritische Schriften II: 1757–1777', 1),
 (3, 'Kritik der reinen Vernunft (2. Aufl., 1787)', 1),
@@ -7,13 +7,15 @@ INSERT INTO TABLE volumes(id, title, section) VALUES
 (6, 'Die Religion innerhalb der Grenzen der blossen Vernunft, Die Metaphysik der Sitten', 1),
 (7, 'Der Streit der Fakultäten, Anthropologie in pragmatischer Hinsicht', 1),
 (8, 'Abhandlungen nach 1781', 1),
-(9, 'Logik. Physische Geographie, Pädagogik', 1),
+(9, 'Logik. Physische Geographie, Pädagogik', 1);
 
+INSERT INTO volumes(id, title, section) VALUES 
 (10, 'Briefe, 1747-1788 (2. Aufl)', 2),
 (11, 'Briefe, 1789-1794 (2. Aufl.)', 2),
 (12, 'Briefe, 1795-1803, Anhang (2. Aufl.)', 2),
-(13, 'Anmerkungen und Register', 2),
+(13, 'Anmerkungen und Register', 2);
 
+INSERT INTO volumes(id, title, section) VALUES 
 (14, 'Mathematik, Physik und Chemie, Physische Geographie', 3),
 (15, 'Anthropologie', 3),
 (16, 'Logik', 3),
@@ -23,8 +25,9 @@ INSERT INTO TABLE volumes(id, title, section) VALUES
 (20, 'Vorarbeiten und Nachträge, Einleitung und Erläuterungen', 3),
 (21, 'Opus postumum (Convolut I bis VI)', 3),
 (22, 'Opus postumum (Convolut VII bis XIII)', 3),
-(23, 'Vorarbeiten und Nachträge, Einleitung und Erläuterungen', 3),
+(23, 'Vorarbeiten und Nachträge, Einleitung und Erläuterungen', 3);
 
+-- INSERT INTO volumes(id, title, section) VALUES 
 -- (24, 'Vorlesungen über Logik', 4),
 -- (25, 'Vorlesungen über Anthropologie', 4),
 -- (26, 'Vorlesungen über Physische Geographie', 4),
@@ -32,8 +35,8 @@ INSERT INTO TABLE volumes(id, title, section) VALUES
 -- (28, 'Vorlesungen über Metaphysik und Rationaltheologie', 4),
 -- (29, 'Kleinere Vorlesungen (Enzyklopädie, Mathematik, Physik) und Ergänzungen', 4);
 
--- Abteilung I: Werke
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+-- Abteilung I: Werke ----------------------------------------------------------
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Gedanken von der wahren Schätzung der lebendigen Kräfte und Beurtheilung der Beweise, deren sich Herr von Leibniz und andere Mechaniker in dieser Streitsache bedient haben, nebst einigen vorhergehenden Betrachtungen, welche die Kraft der Körper überhaupt betreffen', 'GSK', 1, 0, '1746/49'),
 ('Gedanken von der wahren Schätzung der lebendigen Kräfte und Beurtheilung der Beweise, deren sich Herr von Leibniz und andere Mechaniker in dieser Streitsache bedient haben, nebst einigen vorhergehenden Betrachtungen, welche die Kraft der Körper überhaupt betreffen, 1746/49', 'UFE', 1, 1, '1754'),
 ('Die Frage, ob die Erde veralte, physikalisch erwogen', 'FE', 1, 2, '1754'),
@@ -45,9 +48,9 @@ INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES
 ('Fortgesetzte Betrachtung der seit einiger Zeit wahrgenommenen Erderschütterungen', 'FBZE', 1, 8, '1756'),
 ('Metaphysicae cum geometria iunctae usus in philosophia naturali, cuius specimen I. continet monadologiam physicam', 'MonPh', 1, 9, '1756'),
 ('Neue Anmerkungen zur Erläuterung der Theorie der Winde', 'TW', 1, 10, '1756'),
-('Einleitung in die Abtheilung der Werke', NULL, 1, 11, NULL),
+('Einleitung in die Abtheilung der Werke', NULL, 1, 11, NULL);
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Entwurf und Ankündigung eines Collegii der physischen Geographie nebst dem Anhange einer kurzen Betrachtung über die Frage: Ob die Westwinde in unsern Gegenden darum feucht seien, weil sie über ein großes Meer streichen', 'EACG', 2, 0, '1757'),
 ('Neuer Lehrbegriff der Bewegung und Ruhe und der damit verknüpften Folgerungen in den ersten Gründen der Naturwissenschaft', 'NLBR', 2, 1, '1758'),
 ('Versuch einiger Betrachtungen über den Optimismus', 'VBO', 2, 2, '1759'),
@@ -67,76 +70,73 @@ INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES
 ('Von den verschiedenen Racen der Menschen', 'VvRM', 2, 16, '1775'),
 ('Aufsätze, das Philanthropin betreffend', 'AP', 2, 17, '1776-1777');
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
-('Kritik der reinen Vernunft B', 'B', 3, 0, '1787');
-
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Kritik der reinen Vernunft A', 'A', 4, 0, '1781'),
 ('Prolegomena', 'Prol', 4, 1, '1783'),
 ('Grundlegung zur Metaphysik der Sitten', 'GMS', 4, 2, '1785'),
 ('Metaphysische Anfangsgründe der Naturwissenschaft', 'MAN', 4, 3, '1786');
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Kritik der praktischen Vernunft', 'KpV', 5, 0, '1788'),
 ('Kritik der Urtheilskraft', 'KU', 5, 1, '1790');
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Die Religion innerhalb der Grenzen der bloßen Vernunft', 'RGV', 6, 0, '1793'),
 ('Die Metaphysik der Sitten', 'MS', 6, 1, '1797');
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Der Streit der Facultäten', 'SF', 7, 0, '1798'),
 ('Anthropologie in pragmatischer Hinsicht', 'Anth', 7, 1, '1798');
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Anzeige des Lambert''schen Briefwechsels', NULL, 8, 0, '1782'),
-('Nachricht an Ärzte', NULL, 8, 1, '1782');
-('Recension von Schulz''s Versuch einer Anleitung zur Sittenlehre für alle Menschen, ohne Unterschied der Religion, nebst einem Anhange von den Todesstrafen', 'RezSchulz', 8, 2, '1783/90');
-('Idee zu einer allgemeinen Geschichte in weltbürgerlicher Absicht', NULL, 8, 3, '1784');
-('Beantwortung der Frage: Was ist Aufklärung?', NULL, 8, 4, '1784');
-('Recension von J. G. Herders Ideen zur Philosophie der Geschichte der Menschheit', 'RezHerder', 8, 5, '1785');
-('Über die Vulkane im Monde', NULL, 8, 6, '1785');
-('Von der Unrechtmäßigkeit des Büchernachdrucks', 'VUB', 8, 7, '1785');
-('Bestimmung des Begriffs einer Menschenrace', NULL, 8, 8, '1785');
-('Muthmaßlicher Anfang der Menschengeschichte', NULL, 8, 9, '1786');
-('Recension von Gottlieb Hufeland''s Versuch über den Grundsatz des Naturrechts', 'RezHufeland', 8, 10, '1786');
-('Was heißt: Sich im Denken orientiren?', 'WDO', 8, 11, '1786');
-('Einige Bemerkungen zu L. H. Jakob''s Prüfung der Mendelssohn''schen Morgenstunden', NULL, 8, 12, '1786');
-('Über den Gebrauch teleologischer Principien in der Philosophie', 'ÜGTP', 8, 13, '1788');
-('Über eine Entdeckung, nach der alle neue Kritik der reinen Vernunft durch eine ältere entbehrlich gemacht werden soll', 'ÜE', 8, 14, '1790');
-('Über das Mißlingen aller philosophischen Versuche in der Theodicee', NULL, 8, 15, '1791');
-('Über den Gemeinspruch: Das mag in der Theorie richtig sein, taugt aber nicht für die Praxis', 'TP', 8, 16, '1793');
-('Etwas über den Einfluß des Mondes auf die Witterung', NULL, 8, 17, '1794');
-('Das Ende aller Dinge', NULL, 8, 18, '1794');
-('Zum ewigen Frieden. Ein philosophischer Entwurf', 'ZeF', 8, 19, '1795');
-('Von einem neuerdings erhobenen vornehmen Ton in der Philosophie', 'VT', 8, 20, '1796');
-('Ausgleichung eines auf Mißverstand beruhenden mathematischen Streits', NULL, 8, 21, '1796');
-('Verkündigung des nahen Abschlusses eines Tractats zum ewigen Frieden in der Philosophie', 'VNAEF', 8, 22, '1796');
-('Über ein vermeintes Recht aus Menschenliebe zu lügen', 'VRML', 8, 23, '1797');
-('Über die Buchmacherei', NULL, 8, 24, '1798');
-('Vorrede zu Reinhold Bernhard Jachmanns Prüfung der Kantischen Religionsphilosophie', NULL, 8, 25, '1800');
-('Nachschrift zu Christian Gottlieb Mielckes Littauisch-deutschem und deutsch-littauischem Wörterbuch', NULL, 8, 26, '1800');
-('Nachtrag (Band 8)', NULL, 8, 27, NULL);
-('Recension von Silberschlags Schrift: Theorie der am 23. Juli 1762 erschienen Feuerkugel', NULL, 8, 28, '1764');
-('Anhang (Band 8)', NULL, 8, 29, NULL);
+('Nachricht an Ärzte', NULL, 8, 1, '1782'),
+('Recension von Schulz''s Versuch einer Anleitung zur Sittenlehre für alle Menschen, ohne Unterschied der Religion, nebst einem Anhange von den Todesstrafen', 'RezSchulz', 8, 2, '1783/90'),
+('Idee zu einer allgemeinen Geschichte in weltbürgerlicher Absicht', NULL, 8, 3, '1784'),
+('Beantwortung der Frage: Was ist Aufklärung?', NULL, 8, 4, '1784'),
+('Recension von J. G. Herders Ideen zur Philosophie der Geschichte der Menschheit', 'RezHerder', 8, 5, '1785'),
+('Über die Vulkane im Monde', NULL, 8, 6, '1785'),
+('Von der Unrechtmäßigkeit des Büchernachdrucks', 'VUB', 8, 7, '1785'),
+('Bestimmung des Begriffs einer Menschenrace', NULL, 8, 8, '1785'),
+('Muthmaßlicher Anfang der Menschengeschichte', NULL, 8, 9, '1786'),
+('Recension von Gottlieb Hufeland''s Versuch über den Grundsatz des Naturrechts', 'RezHufeland', 8, 10, '1786'),
+('Was heißt: Sich im Denken orientiren?', 'WDO', 8, 11, '1786'),
+('Einige Bemerkungen zu L. H. Jakob''s Prüfung der Mendelssohn''schen Morgenstunden', NULL, 8, 12, '1786'),
+('Über den Gebrauch teleologischer Principien in der Philosophie', 'ÜGTP', 8, 13, '1788'),
+('Über eine Entdeckung, nach der alle neue Kritik der reinen Vernunft durch eine ältere entbehrlich gemacht werden soll', 'ÜE', 8, 14, '1790'),
+('Über das Mißlingen aller philosophischen Versuche in der Theodicee', NULL, 8, 15, '1791'),
+('Über den Gemeinspruch: Das mag in der Theorie richtig sein, taugt aber nicht für die Praxis', 'TP', 8, 16, '1793'),
+('Etwas über den Einfluß des Mondes auf die Witterung', NULL, 8, 17, '1794'),
+('Das Ende aller Dinge', NULL, 8, 18, '1794'),
+('Zum ewigen Frieden. Ein philosophischer Entwurf', 'ZeF', 8, 19, '1795'),
+('Von einem neuerdings erhobenen vornehmen Ton in der Philosophie', 'VT', 8, 20, '1796'),
+('Ausgleichung eines auf Mißverstand beruhenden mathematischen Streits', NULL, 8, 21, '1796'),
+('Verkündigung des nahen Abschlusses eines Tractats zum ewigen Frieden in der Philosophie', 'VNAEF', 8, 22, '1796'),
+('Über ein vermeintes Recht aus Menschenliebe zu lügen', 'VRML', 8, 23, '1797'),
+('Über die Buchmacherei', NULL, 8, 24, '1798'),
+('Vorrede zu Reinhold Bernhard Jachmanns Prüfung der Kantischen Religionsphilosophie', NULL, 8, 25, '1800'),
+('Nachschrift zu Christian Gottlieb Mielckes Littauisch-deutschem und deutsch-littauischem Wörterbuch', NULL, 8, 26, '1800'),
+('Nachtrag (Band 8)', NULL, 8, 27, NULL),
+('Recension von Silberschlags Schrift: Theorie der am 23. Juli 1762 erschienen Feuerkugel', NULL, 8, 28, '1764'),
+('Anhang (Band 8)', NULL, 8, 29, NULL),
 ('Kraus'' Recension von Ulrich''s Eleutheriologie', 'RezUlrich', 8, 30, '1788');
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Logik', 'Log', 9, 0, '1800'),
 ('Physische Geographie', 'PG', 9, 1, '1802'),
 ('Pädagogik', 'Päd', 9, 2, '1803');
 
--- Abteilung II: Briefwechsel
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+-- Abteilung II: Briefwechsel --------------------------------------------------
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Briefe 001-342', 'Br', 10, 0, '1747-1788');
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Briefe 343-647', 'Br', 11, 0, '1789-1794');
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Briefe 648-903', 'Br', 12, 0, '1795-1803');
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Einleitung in die Abteilung: Kant''s Briefwechsel', 'Br', 13, 0, NULL),
 ('Anmerkungen zu Band 10', 'Br', 13, 1, NULL),
 ('Anmerkungen zu Band 11', 'Br', 13, 2, NULL),
@@ -145,15 +145,15 @@ INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES
 ('Allgemeines Register', 'Br', 13, 5, NULL),
 ('Register zu: Kant', 'Br', 13, 6, NULL);
 
--- Abteilung III: Handschriftlicher Nachlass
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+-- Abteilung III: Handschriftlicher Nachlass -----------------------------------
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Einleitung in die Abtheilung des handschriftlichen Nachlasses', 'HN', 14, 0, NULL),
 ('Reflexionen zur Mathematik, Nr. 0001-0019', 'Refl', 14, 1, NULL),
 ('Reflexionen zur Physik und Chemie, Nr. 0020-0082', 'Refl', 14, 2, NULL),
 ('Reflexionen zur physischen Geographie, Nr. 0083-0110', 'Refl', 14, 3, NULL),
 ('Berichtigungen und Nachträge zu Band 14', 'HN', 14, 4, NULL);
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Erläuterungen zur „Psychologia empirica“ in A. G. Baumgartens „Metaphysica“, Nr. 0111-0158', 'HN', 15, 0, '1757'),
 ('Reflexionen zur Anthropologie, Nr. 0158a-1481', 'Refl', 15, 1, NULL),
 ('Entwürfe zu dem Colleg über Anthropologie aus den 70er und 80er Jahren, Nr. 1482-1524', 'HN', 15, 2, '1770-1789'),
@@ -161,22 +161,22 @@ INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES
 ('Zweiter Anhang: Medicin, Nr. 1526-1561', 'HN', 15, 4, NULL),
 ('Berichtigungen und Nachträge zu Band 15', 'HN', 15, 5, NULL);
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Reflexionen zu G. Fr. Meiers „Auszug aus der Vernunftlehre“, Nr. 1562-3488', 'Refl', 16, 0, '1752'),
 ('Berichtigungen und Nachträge', 'HN', 16, 1, NULL);
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Erläuterungen zu A. G. Baumgartens „Metaphysica“, Nr. 3489-3702', 'HN', 17, 0, '1757'),
 ('Reflexionen zur Metaphysik, Nr. 3703-4846', 'Refl', 17, 1, '1753-1776'),
 ('Berichtigungen und Nachträge zu Band 17', 'HN', 17, 2, NULL);
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Reflexionen zur Metaphysik (), Nr. 4847-6205', 'Refl', 18, 0, '1776-1789'),
 ('Bemerkungen Kants in seinem Handexemplar von Eberhards „Vorbereitung zur natürlichen Theologie“, Nr. 6206-6310', 'HN', 18, 1, '1781'),
 ('Reflexionen zur Metaphysik, Nr. 6311-6455', 'Refl', 18, 2, '1790-1804'),
 ('Berichtigungen und Nachträge zu Band 18', 'HN', 18, 3, NULL);
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Erläuterungen Kants zu A. G. Baumgartens „Initia philosophiae practicae primae“, Nr. 6456–6576', 'HN', 19, 0, '1760'),
 ('Reflexionen zur Moralphilosophie, Nr. 6577–7322', 'Refl', 19, 1, NULL),
 ('Erläuterungen Kants zu G. Achenwalls „Iuris naturalis pars posterior“, Nr. 7323–7520', 'HN', 19, 2, '1763'),
@@ -184,28 +184,28 @@ INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES
 ('Reflexionen zur Religionsphilosophie, Nr. 8081–8112', 'Refl', 19, 4, NULL),
 ('Berichtigungen und Nachträge zu Band 19', 'HN', 19, 5, NULL);
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Bemerkungen zu den Beobachtungen über das Gefühl des Schönen und Erhabenen', 'HN', 20, 0, NULL),
 ('Lose Blätter zu den Beobachtungen über das Gefühl des Schönen und Erhabenen', 'HN', 20, 1, NULL),
 ('Erste Einleitung in die Kritik der Urteilskraft', 'HN', 20, 2, NULL),
 ('Preisschrift über die Fortschritte der Metaphysik', 'HN', 20, 3, NULL),
 ('Lose Blätter zu den Fortschritten der Metaphysik', 'HN', 20, 4, NULL),
-('Vorarbeiten zur Schrift gegen Eberhard', 'HN', 20, 5, NULL);
-('Zur Rezension von Eberhards Magazin', 'HN', 20, 6, NULL);
-('Vorredeentwürfe zur Religionsphilosophie', 'HN', 20, 7, NULL);
-('Bemerkungen zur Rechtslehre', 'HN', 20, 8, NULL);
-('Anhang', 'HN', 20, 9, NULL);
-('Einleitung', 'HN', 20, 10, NULL);
-('Erläuterungen', 'HN', 20, 11, NULL);
+('Vorarbeiten zur Schrift gegen Eberhard', 'HN', 20, 5, NULL),
+('Zur Rezension von Eberhards Magazin', 'HN', 20, 6, NULL),
+('Vorredeentwürfe zur Religionsphilosophie', 'HN', 20, 7, NULL),
+('Bemerkungen zur Rechtslehre', 'HN', 20, 8, NULL),
+('Anhang zu Ban 20', 'HN', 20, 9, NULL),
+('Einleitung zu Band 20', 'HN', 20, 10, NULL),
+('Erläuterungen zu Band 20', 'HN', 20, 11, NULL);
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
-('Opus postumum, Erste Hälfte (Convolut I bis VI)', 'OP', 21, 0, NULL),
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
+('Opus postumum, Erste Hälfte (Convolut I bis VI)', 'OP', 21, 0, NULL);
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Opus postumum, Zweite Hälfte (Convolut VII bis XIII)', 'OP', 22, 0, NULL),
-('Index, Einleitung und Erläuterungen zu Band 21 und 22', 'OP', 22, 1, NULL),
+('Index, Einleitung und Erläuterungen zu Band 21 und 22', 'OP', 22, 1, NULL);
 
-INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES 
+INSERT INTO works(title, abbreviation, volume_id, ordinal, year) VALUES 
 ('Fragment zur Preisfrage von 1754', 'OP', 23, 0, NULL),
 ('Vorredeentwurf zur Allgemeinen Naturgeschichte und Theorie des Himmels', 'OP', 23, 1, NULL),
 ('Nachträge zur Kritik der reinen Vernunft', 'OP', 23, 2, NULL),
@@ -229,10 +229,9 @@ INSERT INTO TABLE works(title, abbreviation, volume_id, ordinal, year) VALUES
 ('Ergänzungen zum Opus postumum', 'OP', 23, 20, NULL),
 ('Ergänzungen zum Briefwechsel', 'OP', 23, 21, (1777-1802)),
 ('Ergänzungen zu den Stammbuchblättern', 'OP', 23, 22, NULL),
-('Anhang', 'OP', 23, 23, NULL),
-('Einleitung', 'OP', 23, 24, NULL),
-('Erläuterungen', 'OP', 23, 25, NULL),
+('Anhang zu Band 23', 'OP', 23, 23, NULL),
+('Einleitung zu Band 23', 'OP', 23, 24, NULL),
+('Erläuterungen zu Band 23', 'OP', 23, 25, NULL),
 ('Erläuterungen zum Briefwechsel', 'OP', 23, 26, NULL),
 ('Verzeichnis der Losen Blätter Reickes nach ihrem Abdruck in der Akademieausgabe', 'OP', 23, 27, NULL),
-('Berichtigungen. Uneingereichtes', 'OP', 23, 28, NULL),
-
+('Berichtigungen. Uneingereichtes', 'OP', 23, 28, NULL);
